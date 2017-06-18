83da81e9203658c4b1da5bc094fd24b1f37e08f3
yariki <iyariki.ya@gmail.com>
2017-06-18 12:13:56 +0300

    init commit

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HUDManager : MonoBehaviour 
+{
+
+    NetworkManager netwokManager;
+
+	// Use this for initialization
+	void Start () 
+    {
+        netwokManager = GetComponent<NetworkManager>();
+	}
+	
+
+    public void StartHost()
+    {
+        if(netwokManager)
+        {
+            Debug.Log("Host starting " + Time.timeSinceLevelLoad);    
+            netwokManager.StartHost();
+        }    
+    }
+
+    void OnStartHost()
+    {
+        Debug.Log("Host started " + Time.timeSinceLevelLoad);    
+    }
+	
+}
